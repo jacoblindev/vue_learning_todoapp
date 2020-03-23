d8bf7c784c97576ead01204dd3103559e67f25a9
Jacob Lin <jacoblindev@gmail.com>
2020-03-23 14:11:34 +0800

    Finished the course!

---
+++ src/components/Todoitem.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{ todo.title }}
+            <button v-on:click="$emit('del-todo', todo.id)" class="del">X</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"],
+    methods: {
+        markComplete() {
+            this.todo.completed = !this.todo.completed;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .todo-item {
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px dotted #ccc;
+    }
+
+    .is-complete {
+        text-decoration: line-through;
+    }
+
+    .del {
+        background: #f00;
+        color: #fff;
+        border: none;
+        padding: 8px;
+        border-radius: 3px;
+        cursor: pointer;
+        float: right;
+    }
+    
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <h3>This is the ToDoList app v1.0.0</h3>
+    <p>It's a follow alone project from YouTube "Vue Crash Course" by Traversy Media.</p>
+  </div>
+</template>
