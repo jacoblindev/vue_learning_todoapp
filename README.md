93e8d6e0115d40e975564d8e6563f81af87dc61a
Jacob Lin <jacoblindev@gmail.com>
2022-02-05 08:45:21 +0800

    chore: update package & clean up

--- README.md
+++ README.md
@@ -1,24 +1,29 @@
 # vue_learning_todoapp
 
 ## Project setup
-```
+
+``` js
 npm install
 ```
 
 ### Compiles and hot-reloads for development
-```
+
+``` js
 npm run serve
 ```
 
 ### Compiles and minifies for production
-```
+
+``` js
 npm run build
 ```
 
 ### Lints and fixes files
-```
+
+``` js
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="about">
-    <h1>About</h1>
-    <h3>This is the ToDoList app v1.0.0</h3>
+    <h2>This is the ToDoList app v1.0.0</h2>
     <p>It's a follow alone project from YouTube "Vue Crash Course" by Traversy Media.</p>
   </div>
 </template>
+
+<style scoped>
+.about {
+    margin: 1em auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
